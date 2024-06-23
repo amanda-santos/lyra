6e8179d60687c0f3c41ef41d65b4b73240c77815
amanda-santos <amandasf0200@gmail.com>
2024-06-23 18:23:51 -0300

    feat: :lipstick: setup color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,21 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      "primary-color-500": "#8A42DB",
+      "primary-color-400": "#9955E8",
+      "secondary-color": "#7DFFAF",
+
+      "surface-background": "#171023",
+      "surface-primary": "#2C243B",
+      "surface-secondary": "#413A4F",
+      "surface-tertiary": "#4E4563",
+
+      "text-primary": "#F5F6F6",
+      "text-secondary": "#CDD0D4",
     },
   },
   plugins: [],
 };
+
 export default config;
