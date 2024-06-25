22cc3fec06f9c4e4c1ea16feab4071efe178c6a0
amanda-santos <amandasf0200@gmail.com>
2024-06-25 20:00:49 -0300

    feat: add hero section action buttons

--- src/app/components/navbar-item.tsx
+++ src/app/components/navbar-item.tsx
@@ -1,5 +1,5 @@
 import classnames from "classnames";
-import { ComponentProps, ReactNode } from "react";
+import { ComponentProps } from "react";
 
 type NavbarItemProps = ComponentProps<"a"> & {
   isActive?: boolean;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { ArrowRight, ReadCvLogo } from "@phosphor-icons/react/dist/ssr";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-surface-background px-10">
-      <div className="flex w-full flex-col items-center justify-between gap-6 px-10 py-24 md:flex-row-reverse md:px-16">
+      <div className="flex w-full flex-col items-center justify-between gap-6 py-24 md:flex-row-reverse lg:px-16">
         <Image
           src="/programmer-illustration.svg"
           alt="Logo"
@@ -25,6 +26,18 @@ export default function Home() {
               such as React, React Native, and Node.js.
             </p>
           </span>
+
+          <div className="flex items-center gap-4">
+            <button className="flex items-center gap-2 rounded-lg bg-surface-secondary px-6 py-4 font-body font-medium text-text-primary transition-colors hover:bg-surface-tertiary focus:outline-offset-4 focus:outline-text-primary">
+              <ReadCvLogo size={24} />
+              My resume
+            </button>
+
+            <button className="flex items-center gap-2 rounded-lg bg-primary-color-500 px-6 py-4 font-body font-medium text-text-primary transition-colors hover:bg-primary-color-400 focus:outline-offset-4 focus:outline-text-primary">
+              Get in touch
+              <ArrowRight size={24} />
+            </button>
+          </div>
         </div>
       </div>
     </main>
